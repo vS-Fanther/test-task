41db21c30a24690022d935e5b235d8b073b5a0d2
vS-Fanther <50148623+vS-Fanther@users.noreply.github.com>
2024-07-19 09:45:56 +0300

    Removed unique from Company name

--- database/migrations/2024_07_17_084020_create_companies_table.php
+++ database/migrations/2024_07_17_084020_create_companies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->integer('id')->autoIncrement()->unique();
-            $table->string('name')->unique('company_names_index');
+            $table->string('name');
             $table->string('email');
             $table->integer('country_id');
             $table->foreign('country_id')->references('id')->on('countries');
